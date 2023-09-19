9ee1ea030844cb0ff7ad2358e773155e30221bd3
Jackson Londoño <alexisjacksonlon213@gmail.com>
2023-09-19 09:23:50 -0500

    actualizado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,16 @@ export const Navbar = () => {
   return (
     <nav className='menu'>
         <ul>
-            <List path="/" name="Home" ></List>
-            <List path="/About" name="About"></List>
-            <List path="/Contact" name="Contact"></List>
-            <List path="/Compras" name="Compras"></List>
+          <List name="Home" path={'/'} />
+          <List name="About" path={'/About'} />
+          <List name="Contact" path={'/Contact'} />
+          <List name="Compras" path={'/Compras'} />
+
         </ul>
     </nav>
   )
 }
+{/* <List path="/" name="Home" ></List>
+<List path="/About" name="About"></List>
+<List path="/Contact" name="Contact"></List>
+<List path="/Compras" name="Compras"></List> */}
