29bf651935a07043807c82ed017a282008a2c8cb
Jackson Londoño <alexisjacksonlon213@gmail.com>
2023-09-19 10:25:13 -0500

    actualizo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './About.css';
-const img ='../../public/images/bleux.png'
-const img2 ='../../public/images/blueasy.png'
-const img3 ='../../public/images/gifs.png'
 
 
 export default function About () {
@@ -27,7 +24,7 @@ export default function About () {
                     <h3>BLEUX
                         <hr className="hrProyect1" />
                     </h3>
-                    <img onClick={abrirProyect1} src={img} alt="" />
+                    <img onClick={abrirProyect1} src="https://cdn.discordapp.com/attachments/1089979203534999663/1153712104466038924/Captura_de_pantalla_2023-09-19_101601.png" alt="" />
                     <p>Esta página se centra en promocionar un producto o servicio, destacando sus características y opciones de precios. La información proporcionada es de relleno ("Lorem ipsum"), pero la estructura general de la página sugiere su propósito de promoción y ventas.</p>
                 </div>
 
@@ -35,7 +32,7 @@ export default function About () {
                     <h3>BLUEASY
                     <hr className="hrProyect2" />
                     </h3>
-                    <img onClick={abrirProyect2} src={img2} alt="" />
+                    <img onClick={abrirProyect2} src="https://cdn.discordapp.com/attachments/1089979203534999663/1153712634189840384/Captura_de_pantalla_2023-09-19_102141.png" alt="" />
                     <p>la página web "blueasy" es un sitio multipropósito con secciones que presentan información sobre servicios, un portafolio de imágenes, información de contacto y testimonios. Sin embargo, el contenido actual es principalmente de relleno.</p>
                 </div>
 
@@ -44,7 +41,7 @@ export default function About () {
                     <hr className="hrProyect3" />
 
                     </h3>
-                    <img onClick={abrirProyect3} src={img3} alt="" />
+                    <img onClick={abrirProyect3} src="https://cdn.discordapp.com/attachments/1089979203534999663/1153713074604355584/Captura_de_pantalla_2023-09-19_102330.png" alt="" />
                     <p>esta página llamada "Gifs" permite a los usuarios buscar y ver GIFs, mostrando tanto GIFs tendencia como resultados de búsqueda en dos secciones separadas. </p>
                 </div>
 
